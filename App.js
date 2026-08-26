a5ed686c5ff92aa17ee61e62a93b0ef048904110
Ahmad Alshaqlde <ahmad.alshaqlde2003@gmail.com>
2026-08-26 15:34:30 +0300

    Add Node.js project

--- App.js
+++ App.js
@@ -1,1 +1,11 @@
-console.log("ahmad");
+const http = require("node:http");
+// console.log(http);
+
+const Server=http.createServer((req,res)=>{
+    console.log(req);
+    // console.log("serveris runing")
+return;
+});
+
+Server.listen(8080,()=>console.log("server is runing on PORT: 8080"));
+
